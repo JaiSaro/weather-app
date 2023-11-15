e3d051a86087e183c425a99a86a88a64790321c3
Jai Saravanan <jaisaravanan.cs@gmail.com>
2023-11-15 05:32:41 +0530

    Added the base component and created structure for the application.

---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+const Headers = () => {
+    return (
+        <header className='bg-white'>
+            <nav
+                className='mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8'
+                aria-label='Global'
+            >
+                <div className='flex justify-center lg:flex-1'>
+                    <a href='#' className='-m-1.5 p-1.5'>
+                        <span className='sr-only'>Weather Assist</span>
+                        <img
+                            className='h-8 w-auto'
+                            src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600'
+                            alt='Weather Assist'
+                        />
+                    </a>
+                    <a href='#' className='-m-1.5 p-1.5'>
+                      <span className="logo-color text-2xl ml-2">Weather Assists</span>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Headers;
